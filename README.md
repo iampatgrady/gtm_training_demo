22eb43ba792dd2ffa3af9998d3d06f0267828ebc
Pat Grady <pat@macbook-pro.lan>
2020-07-20 16:56:01 -0500

    updated readme

--- README.md
+++ README.md
@@ -13,6 +13,14 @@ npm test
 npm start
 ```
 
+## ADD GTM
+Edit line 1 of the `app.js` to include your GTM container ID:  
+```JavaScript
+const gtm_container_id = "GTM-REPLACE"
+// becomes
+const gtm_container_id = "GTM-M4RX7C7"
+```
+
 ## DEMO
 Open a browser to: http://localhost:8080  
 Run demo
--- app.js
+++ app.js
@@ -1,20 +1,19 @@
+const gtm_container_id = "GTM-REPLACE"
 const express = require('express');
 const path = require("path");
 const favicon = require('express-favicon');
-
-
+const PORT = process.env.PORT || '8080';
 const app = express();
+
 app.use(express.static(path.join(__dirname, "public")));
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 app.get('/', (req,res,next) => {
-  res.render("index", {section: "GTM Training", name: "Homepage", container: "GTM-M4RX7C7"})
+  res.render("index", {section: "GTM Training", name: "Homepage", container: gtm_container_id })
 });
 
-
-const PORT = process.env.PORT || '8080';
 if (process.env.NODE_ENV !== 'test') {
   app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
